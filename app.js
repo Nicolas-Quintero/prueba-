fced8badfbb8f31e6f3db2c6784d9fa6a2aee3b3
nico <nicolasquintero151@gmail.com>
2022-07-25 21:37:28 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {resolve} = require('path');
+const app = express();
+const port = process.env.PORT || 2020;
+app.listen(port);
+const public = resolve(__dirname, 'public');
+const static = express.static(public);
+app.use(static);
+
+app.get('/', (req, res) => {
+  let view = resolve(__dirname, 'views','index.html');
+  return res.sendFile(view);
+}) 
